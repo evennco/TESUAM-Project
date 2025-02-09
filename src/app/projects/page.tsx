3b1eac06856ab75980489b913b3e5ae6ef8668e7
Jonathan Betancur <jonathanbetancurespinosa@gmail.com>
2025-02-09 14:10:59 -0500

    projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,9 +1,26 @@
-export default function Projects() {
+import Carousel from '@/components/ui/homepage/carrousel';
+import InfiniteCarousel from '@/components/ui/homepage/infinitecarrousel';
+import StadisticsProjects from '@/components/ui/projects/stadisticsprojects';
+import Stories from '@/components/ui/homepage/stories';
+import Volunteer from '@/components/ui/homepage/volunteer';
+import Faq from '@/components/ui/homepage/faq';
+import Contact from '@/components/ui/homepage/contact';
+import Projects from '@/components/ui/homepage/projects';
+import { carrouseldata } from '@/lib/utils';
+import { Suspense } from 'react';
+import LazyLoadComponent from '@/components/ui/common/LazyLoadComponent';
+import Projectsbanner from '@/components/ui/projects/projectsbanner';
+
+export default function Projectspage() {
     return (
       <main className="flex flex-col text-center justify-center items-center">
         <div className="w-full flex-grow">
-          <h1 className="text-4xl font-bold">Projects</h1>
-          <p className="mt-4 text-lg">This is the Projects page of our website.</p>
+            <Projectsbanner />
+            <StadisticsProjects />
+            <Projects />
+            <Projects />
+            <Projects />
+            <Volunteer />
         </div>
       </main>
     );
