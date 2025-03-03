51da4483618f5eca37d7b99365a78d8edd7c4cba
Jonathan Betancur <jonathanbetancurespinosa@gmail.com>
2025-03-03 18:03:23 -0500

    contact page form fixes

--- src/components/ui/common/contactform.tsx
+++ src/components/ui/common/contactform.tsx
@@ -22,7 +22,7 @@ const ContactForm: React.FC = () => {
                     <textarea id="message" rows={2} className="resize-none field-sizing-content mt-1 block w-full p-4 border border-gray-300 rounded-3xl shadow-sm dark:placeholder-gray-400 dark:text-white bg-[#D9D9D9]"></textarea>
                 </div>
                 <div className="flex items-center justify-center">
-                <button type="submit" className="bg-foundationcolorred1 text-white px-6 py-3 rounded-full hover:text-foundationcoloryellow2 transition-colors w-1/3 items-center justify-center">Enviar</button>
+                    <button type="submit" className="bg-foundationcolorred1 text-white px-6 py-3 rounded-full hover:text-foundationcoloryellow2 transition-colors w-1/3 items-center justify-center">Enviar</button>
                 </div>
             </form>
         </div>
