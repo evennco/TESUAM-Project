eb3c22ef7dcc9285fd6ff653127f2ab167384fce
Jonathan Betancur <jonathanbetancurespinosa@gmail.com>
2025-03-03 20:58:42 -0500

    interactive components

--- src/components/ui/contact/carrousel.tsx
+++ src/components/ui/contact/carrousel.tsx
@@ -1,7 +1,21 @@
+"use client"
+import { useState } from "react";
+
 export default function Carrousel() {
+  const [activeSection, setActiveSection] = useState<string>("");
+
+  const getLinkClass = (section: string) =>
+    activeSection === section
+      ? "bg-foundationcolorred1 text-foundationcoloryellow1 px-6 py-3 rounded-full transition-colors"
+      : "bg-foundationcolorwhite bg-opacity-70 text-foundationcolorred2 px-6 py-3 rounded-full transition-colors hover:bg-[#d3d0cc]";
+
+  const handleClick = (section: string) => {
+    setActiveSection(section);
+  };
+
   return (
     <section className="w-full h-[85vh] bg-foundationcolorred2 text-white flex items-center px-6 lg:px-8">
-      <div className="py-8 px-4max-w-4xl mx-auto">
+      <div className="py-8 px-4 max-w-4xl mx-auto">
         <h1 className="text-4xl sm:text-4xl md:text-5xl font-bold mb-6">
           Conéctate con TESUAM
           <br className="sm:hidden lg:block" />
@@ -14,25 +28,29 @@ export default function Carrousel() {
         <div className="flex flex-wrap gap-4">
           <a
             href="#redes-sociales"
-            className="bg-foundationcolorred1 text-foundationcoloryellow1 px-6 py-3 rounded-full transition-colors"
+            onClick={() => handleClick("redes-sociales")}
+            className={getLinkClass("redes-sociales")}
           >
             Redes Sociales
           </a>
           <a
             href="#enviar-correo"
-            className="bg-foundationcolorwhite bg-opacity-70 text-foundationcolorred2 px-6 py-3 rounded-full transition-colors hover:bg-[#d3d0cc]"
+            onClick={() => handleClick("enviar-correo")}
+            className={getLinkClass("enviar-correo")}
           >
             Enviar un Correo
           </a>
           <a
             href="#boletin"
-            className="bg-foundationcolorwhite bg-opacity-70 text-foundationcolorred1 px-6 py-3 rounded-full transition-colors hover:bg-[#d3d0cc]"
+            onClick={() => handleClick("boletin")}
+            className={getLinkClass("boletin")}
           >
             Suscribirme al Boletín
           </a>
           <a
             href="#pqrsf"
-            className="bg-foundationcolorwhite bg-opacity-70 text-foundationcolorred1 px-6 py-3 rounded-full transition-colors hover:bg-[#d3d0cc]"
+            onClick={() => handleClick("pqrsf")}
+            className={getLinkClass("pqrsf")}
           >
             PQRSF
           </a>
